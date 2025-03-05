b48c72ce0af0671c0d99057ee7708fbb5f563594
MykytaChystyi <neyton61@gmail.com>
2025-03-05 13:19:08 +0200

    Скелет для оплати пакетів

---
+++ Socialized/UseCases/Packages/Command/GetClientTokenForPayCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UseCases.Packages.Command
+{
+    public class GetClientTokenForPayCommand
+    {
+        [Required(ErrorMessage = "User token is required")]
+        public string UserToken { get; set; } = string.Empty;
+    }
+}
+
---
+++ Socialized/UseCases/Packages/IPackageManager.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Packages;
+using Domain.Users;
+using UseCases.Packages.Command;
+
+namespace UseCases.Packages
+{
+    public interface IPackageManager
+    {
+        ServiceAccess CreateDefaultServiceAccess(User user);
+        ServiceAccess CreateDefaultServiceAccess(long userId);
+        ICollection<PackageAccess> GetPackageAccess();
+        ICollection<DiscountPackage> GetDiscountPackageAccess();
+        string GetClientTokenForPay(GetClientTokenForPayCommand command);
+        void PayForPackage(PayForPackageCommand command);
+    }
+}
